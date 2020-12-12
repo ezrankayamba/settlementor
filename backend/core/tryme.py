d3477afef6b678ac20b6644d7b1501cb2bc52566
ezrankayamba <ezrankayamba@gmail.com>
2020-12-12 12:39:54 +0300

    sftp no confirm

--- backend/core/tryme.py
+++ backend/core/tryme.py
@@ -1,6 +1,7 @@
 from core import sftp_connect as sftp
 from config import config as cfg
 
-if __name__ == "__main__":
+
+def try():
     file = 'test.csv'
     cfg.upload(f'{cfg.sftp_local_path}/{file}', f'{cfg.sftp_tigo_path()}/{file}')
