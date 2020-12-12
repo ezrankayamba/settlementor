74cf2d668b78ba35b04c5eae7f65c53d16c3a92a
ezrankayamba <ezrankayamba@gmail.com>
2020-12-12 12:37:06 +0300

    sftp no confirm

--- backend/core/tryme.py
+++ backend/core/tryme.py
@@ -1,4 +1,6 @@
-# with open(f'files/test.csv') as f:
-#     print('File read!')
+from core import sftp_connect as sftp
+from config import config as cfg
 
-sig = 'iRdOLXtCBMiFM0B98jC3Qinnpamt9n0gC2HqbmJgOLKCqBQGx/0ot/nqbbAKqXrx7m3umwfDpuBWcuSuR7DJ0di8IvVguRQja9UVe9q8mEeF40CIB/DC9LiUPLxJZHRfWQegA7DifmFB4S0oJ/MLhTAfs1HH3h/vvnhy4oimZ5ubvs2rKSrlC1HRSgpNXsvEmc0k1t3p1qjkXXHjqC3tyjAmK5+lCqP2eSRcnYZcxxu9OiYm0UNuKlW8AJ7/Avi/M8IrrdvIczvKbCg6ZzZvltQIdnwkJkwxFZe6ODPLyreJ+r6AxjE9cKWlEkdHNj0oyqude4bWD33Cn2+pyaTVnQ\u003d\u003d'
+if __name__ == "__main__":
+    file = 'test.csv'
+    cfg.upload(f'{cfg.sftp_local_path}/{file}', f'{cfg.sftp_tigo_path()}/{file}')
